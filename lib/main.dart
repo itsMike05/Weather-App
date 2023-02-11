53d4e6faa7bcc3dff052200758a10248f991307a
Michał Pelc <michal.pelc@live.com>
2023-02-11 17:25:01 +0100

    Created HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/pages/home_page.dart';
 
 void main() {
   runApp(const WeatherApp());
 }
+
+// (109, 72, 47, 180)
 
 class WeatherApp extends StatelessWidget {
   const WeatherApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Container(
-      color: const Color.fromARGB(109, 72, 47, 180),
-    ));
+    return const MaterialApp(
+      home: HomePage(),
+    );
   }
 }
