1db1ba00d0d6ca5def45a3ba7baa87743f3e8d54
Michał Pelc <michal.pelc@live.com>
2023-02-11 23:50:44 +0100

    Weather API configured

---
+++ lib/services/weather_api.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:weather_app/model/weather.dart';
+
+class WeatherApi {
+  Future<Weather>? getCurrentWeather(String? location) async {
+    var endpoint = Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?q=$location&appid=71c276bd62c8811e1be99b6686411878&units=metric");
+
+    var response = await http.get(endpoint);
+    var weatherBody = jsonDecode(response.body);
+
+    return Weather.fromJson(weatherBody);
+  }
+}
